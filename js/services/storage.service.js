d6b6f30b4491b4d53c332cc1957e9fa57bccdd95
alexeikepov <alexeikepov@gmail.com>
2025-06-05 14:00:15 +0300

    Start API

--- js/services/storage.service.js
+++
@@ -1,8 +0,0 @@
-export function saveToStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
-}
-
-export function loadFromStorage(key) {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : null;
-}
