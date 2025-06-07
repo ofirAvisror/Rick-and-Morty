38423c4772356f492ec6637998894323817ac31d
alexeikepov <alexeikepov@gmail.com>
2025-06-07 19:38:44 +0300

    edit contact page

--- js/pages/contact.page.js
+++ js/pages/contact.page.js
@@ -1,11 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
+  const sendBtn = document.getElementById("sendBtn");
   const form = document.querySelector(".form-contact");
+  const message = document.getElementById("formMessage");
 
-  if (form) {
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-      alert("Your request has been received! Thank you.");
-      form.reset();
-    });
-  }
+  sendBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const text = document.getElementById("message").value.trim();
+
+    if (!name || !email || !phone || !text) {
+      alert("Please fill in all the fields before submitting.");
+      return;
+    }
+
+    message.style.display = "block";
+    form.reset();
+
+    setTimeout(() => {
+      message.style.display = "none";
+    }, 5000);
+  });
 });
